d4f0089d8398c704f9edc25d25ad55d0cfb912df
vikash <vikash.kumar@appzlogic.com>
2021-02-26 12:36:00 +0530

    fixes path related error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static('./tesolo-frontend/dist/'));
+app.use(express.static(__dirname + './tesolo-frontend/dist/'));
 // Start the app by listening on the default
 // Heroku port
 
@@ -30,7 +30,7 @@ app.use(forceSSL());
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
-  res.sendFile('./tesolo-frontend/dist/index.html');
+  res.sendFile(__dirname + './tesolo-frontend/dist/index.html');
 });
 
 app.listen(process.env.PORT || 4200);
